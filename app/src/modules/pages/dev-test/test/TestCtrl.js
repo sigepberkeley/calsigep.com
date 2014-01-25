2ca93c8d284c08983ee2ef64394f2fcae83f114b
Luke Madera <luke.madera@gmail.com>
2014-01-25 14:28:20 -0800

    remove crop testing stuff, fix gallery.less syntax error

--- app/src/modules/pages/dev-test/test/TestCtrl.js
+++ app/src/modules/pages/dev-test/test/TestCtrl.js
@@ -3,41 +3,4 @@
 angular.module('myApp').controller('TestCtrl', ['$scope', '$timeout', 'appHttp', 'UserModel', '$location', function($scope, $timeout, appHttp, UserModel, $location) {
 	
 	$scope.user =UserModel.load();
-	
-	
-	var cropDiv1 = document.getElementById('crop1'),
-          cropDiv2 = document.getElementById('crop2'),
-          crop1,
-          crop2;
-
-      crop1 = new Crop(cropDiv1, {
-        coords: [[100, 100], [400, 400]],
-        onChange: updateCoords
-      });
-      crop2 = new Crop(cropDiv2, {
-        coords: [[440, 0], [640, 200]],
-        onChange: updateCoords
-      });
-
-      cropDiv1.addEventListener('click', function (event) {
-        zoom(event, crop1);
-      });
-      cropDiv2.addEventListener('click', function (event) {
-        zoom(event, crop2);
-      });
-
-      function updateCoords(el, coords) {
-        el.querySelector('.coords').innerText = '(' + coords[0].join(',') + '), (' + coords[1].join(',') + ')';
-      }
-
-      function zoom(event, crop) {
-        var role = event.target.getAttribute('data-role');
-
-        if (role === 'zoomIn') {
-          crop.scaleImage(1.05);
-        } else if (role === 'zoomOut') {
-          crop.scaleImage(0.95);
-        }
-      }
-	  
 }]);
