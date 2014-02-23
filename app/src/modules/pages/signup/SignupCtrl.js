8184bda0501cb6f66113b64d779e14031ec64d84
Matthew Symonds <matthew.symonds@berkeley.edu>
2014-02-22 21:40:54 -0800

    attempting to get Angular to recognize autofill

--- app/src/modules/pages/signup/SignupCtrl.js
+++ app/src/modules/pages/signup/SignupCtrl.js
@@ -21,4 +21,20 @@ angular.module('myApp').controller('SignupCtrl', ['$scope', 'appHttp', 'UserMode
 		});
 	};
 
+	return function(scope, elem, attrs) {
+    // Fixes Chrome bug: https://groups.google.com/forum/#!topic/angular/6NlucSskQjY
+    elem.prop('method', 'POST');
+
+    // Fix autofill issues where Angular doesn't know about autofilled inputs
+    if(attrs.ngSubmit) {
+      setTimeout(function() {
+        elem.unbind('submit').submit(function(e) {
+          e.preventDefault();
+          elem.find('input, textarea, select').trigger('input').trigger('change').trigger('keydown');
+          scope.$apply(attrs.ngSubmit);
+        });
+      }, 0);
+    }
+  };
+
 }]);
