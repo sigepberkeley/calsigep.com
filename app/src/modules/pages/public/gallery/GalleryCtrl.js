e1960a877f99a8e1bf72786bae225d7f9fc6c91b
Matthew Symonds <matthew.symonds@berkeley.edu>
2014-02-22 16:51:04 -0800

    improving gallery

--- app/src/modules/pages/public/gallery/GalleryCtrl.js
+++ app/src/modules/pages/public/gallery/GalleryCtrl.js
@@ -9,7 +9,7 @@ function($scope, appHttp, $timeout)
 	$scope.albums = [];
 	$scope.album_view = false;
 	$scope.cur_album = {'photos': []};		//Currently viewed album
-	
+
 	var search_promise =appHttp.go({}, {url:'photo/searchAlbums', data:  {}  }, {});
 	search_promise.then(
 		function(ret1)
@@ -21,12 +21,12 @@ function($scope, appHttp, $timeout)
 			console.log(ret1.msg);
 		}
 	);
-	
+
 	$scope.viewAlbum = function(index)
 	{
 		$scope.cur_album = $scope.albums[index];
 		$scope.album_view = true;
-		
+
 		if($scope.albums[index].loaded !== true)
 		{
 			//Look up the rest of the album's data
@@ -49,12 +49,12 @@ function($scope, appHttp, $timeout)
 			);
 		}
 	};
-	
+
 	//Carousel
 	$scope.slides = [{'placeholder': ''}];
 	$scope.opts =
 	{
 		'curSlide': 0
 	};
-	
+
 }]);
