c918dd5e98984a6b5867125e252c9f7e14d10712
Stevie Kobori <skobori@gmail.com>
2014-03-19 18:32:44 -0700

    Added some backend calls to the roster js file

--- app/src/modules/pages/roster/RosterCtrl.js
+++ app/src/modules/pages/roster/RosterCtrl.js
@@ -3,7 +3,30 @@
 
 'use strict';
 
-angular.module('myApp').controller('RosterCtrl', ['$scope',
-function($scope) {
+angular.module('myApp').controller('RosterCtrl', ['$scope', 'appHttp',
+function($scope, appHttp) {
 	
+/**
+ *  We're going to need a list of the member information. We can use 
+ *  the ng-repeat directive to build the actual roster. 
+ */
+
+ /**
+  *  Backend calls
+  *  users stores array of user objects from the database
+  */
+	var params = {'fields': {}};
+
+	params.searchString = 'undergrad';
+
+	params.searchFields = ['alumni_status'];
+
+	var promise1 = appHttp.go({}, {url:'user/search', data:params}, {});
+		promise1.then(function(response) {
+			$scope.users = response.result.results;		
+	});
+
+	
+
+
 }]);
